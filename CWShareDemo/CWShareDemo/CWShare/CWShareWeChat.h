6893c213699fd81d12ed179fdba942510fd8a57d
ChrisWang115 <antjun@126.com>
2014-07-29 17:20:25 +0800

    add wechat, remove json lib, change network lib from asihttprequest to afnetworking

---
+++ CWShareDemo/CWShareDemo/CWShare/CWShareWeChat.h
@@ -0,0 +1,13 @@
+//
+//  CWShareWeChat.h
+//  CWShareDemo
+//
+//  Created by Wang Jun on 14-3-26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWShareWeChat : NSObject
+
+@end
---
+++ CWShareDemo/CWShareDemo/CWShare/CWShareWeChat.m
@@ -0,0 +1,13 @@
+//
+//  CWShareWeChat.m
+//  CWShareDemo
+//
+//  Created by Wang Jun on 14-3-26.
+//
+//
+
+#import "CWShareWeChat.h"
+
+@implementation CWShareWeChat
+
+@end
