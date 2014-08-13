ea6038adf4bf13e3e4fb5e608b801fc37bb73796
ChrisWang115 <antjun@126.com>
2014-08-13 12:42:39 +0800

    add tencentOpenAPI header file

---
+++ CWShareDemo/CWShareDemo/CWShare/Tencent/TencentOpenAPI.framework/Headers/TencentOpenSDK.h
@@ -0,0 +1,17 @@
+//
+//  TencentOpenSDK.h
+//  TencentOpenApi_IOS
+//
+//  Created by qqconnnect on 14-7-17.
+//  Copyright (c) 2014年 Tencent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <TencentOpenAPI/QQApi.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+#import <TencentOpenAPI/QQApiInterfaceObject.h>
+#import <TencentOpenAPI/sdkdef.h>
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/TencentOAuthObject.h>
+#import <TencentOpenAPI/WeiBoAPI.h>
+#import <TencentOpenAPI/WeiyunAPI.h>
