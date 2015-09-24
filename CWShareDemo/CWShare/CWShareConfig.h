ce390d215ea380a682f3d07952680d550a317d79
Fiber <wj@jhrx.cn>
2015-09-24 09:13:28 +0800

    replace wechat sdk

--- CWShareDemo/CWShare/CWShareConfig.h
+++ CWShareDemo/CWShare/CWShareConfig.h
@@ -6,13 +6,13 @@
 //
 //
 
-//新浪微博配置 （第三方包：V3.1.1 2015年8月更新）
+//新浪微博配置 （第三方包：V3.1.1 2015年9月更新）
 #define SINA_APP_KEY                @"2565381442"
 #define SINA_REDIRECT_URL           @"http://m.shenghuofan.com"
 
 //腾讯QQ配置 （第三方包：V2.9版本 2015年4月更新）
 #define TENCENT_APP_KEY             @"101151679"
 
-//微信配置 （第三方包：V1.5版本 2015年5月更新）
+//微信配置 （第三方包：V1.6版本 2015年9月更新）
 #define WeChatAppID                 @"wx28bc85e3cfcfb3af"
 #define WechatAppSecret             @"b18854e5c901b1f9d033efb96aab024a"
